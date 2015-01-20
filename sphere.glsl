380bf195c27f8d7b2ab8192e363fd3a9013c4247
Christian Dugenio <christiandugenio@gmail.com>
2015-01-19 19:09:39 -0500

    added sphere vertex shader

---
+++ sphere.glsl
@@ -0,0 +1,32 @@
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+attribute vec4 color;
+attribute vec4 position;
+attribute vec3 normal;
+
+varying vec4 v_color;
+
+void main()
+{
+    float PI = 3.14159265358979323846264;
+
+    vec4 realposition = vec4(1,1,1,1);
+
+    float theta = position.x * 2.0 * PI;
+    float phi = position.y * PI;
+
+    realposition.x = cos(theta) * sin(phi);
+    realposition.y = sin(theta) * sin(phi);
+    realposition.z = cos(phi);
+    realposition.w = 1.0;
+
+    if(position.w == 1.0){
+        gl_Position = projection * view * model * realposition;
+    } else {
+        gl_Position = projection * view * model * (realposition + vec4(normal, 0.0));
+    }
+
+    v_color = color;
+}
